dc56b2ecbb82dbc9afef3f22841bff1bfb35b250
ozzyfromspace <float.circuit@gmail.com>
2022-05-27 10:44:32 +0000

    add basic styles to improve feedback. will improve on it later

--- src/components/Cow/Cow.tsx
+++ src/components/Cow/Cow.tsx
@@ -1,20 +1,22 @@
-// import styles from './Cow.module.scss';
-
 import { Player, PlayerColor, PlayerColorOnPad } from '../../@types/coreTypes';
 import { CowProps } from '../../@types/props';
 import { Typename } from '../../@types/typenames';
 import sendToGameReducer from '../../utils/sendToGameReducer';
 import { useGame } from '../GameProvider/GameProvider';
+import styles from './Cow.module.scss';
+
 
 const Cow = (props: CowProps) => {
-  const { game, dispatch: gameDispatch } = useGame();
   const { centerX, centerY, radius, cowId, owner } = props;
+  const { game, dispatch: gameDispatch } = useGame();
   const defaultFillColor =
     owner === Player.ONE ? PlayerColor.ONE : PlayerColor.TWO;
 
   const cow = game.cows[owner][cowId];
   const cowOnPad =
     cow.__typename === Typename.FREE_COW && game.glowing.pads.has(cow.padId);
+    const isGlowing = (cow.__typename === Typename.FREE_COW && game.glowing.pads.has(cow.padId));
+    const glowingClass = isGlowing ? styles[`cow-glow-${cow.owner}`] : '';
 
   const fillColor = cowOnPad
     ? cow.owner === Player.ONE
@@ -31,6 +33,7 @@ const Cow = (props: CowProps) => {
       cy={centerY}
       fill={fillColor}
       stroke="#777777"
+      className={glowingClass}
       onClick={sendToGameReducer(cowId, gameDispatch)}
     />
   );
--- src/components/Pad/Pad.tsx
+++ src/components/Pad/Pad.tsx
@@ -1,31 +1,16 @@
+// import styles from './Pad.module.scss';
 import { PadProps } from '../../@types/props';
 import sendToGameReducer from '../../utils/sendToGameReducer';
-import { getPlayer } from '../Cows/utils/initCows';
 import { useGame } from '../GameProvider/GameProvider';
-import styles from './Pad.module.scss';
 
 const Pad = (props: PadProps) => {
-  const {
-    centerX,
-    centerY,
-    radius,
-    stroke,
-    fill,
-    strokeOpacity,
-    padId,
-    isGlowing,
-    visitingCowId,
-  } = props;
+  const { centerX, centerY, radius, stroke, fill, strokeOpacity, padId } =
+    props;
 
   const { dispatch: gameDispatch } = useGame();
 
-  const visitingPlayer = !!visitingCowId && getPlayer(visitingCowId);
-
-  const glow = isGlowing && visitingPlayer ? styles[`pad-glow-${visitingPlayer}`] : '';
-
   return (
     <circle
-      className={glow}
       id={padId}
       data-testid={padId}
       cx={centerX}

71bf01f97b8d0194900c6dcc6b18fef172536173
Oswald Kay Chisala <79558128+ozzyfromspace@users.noreply.github.com>
2022-11-08 14:59:09 -0500

    add tests to capture the expected behavior of the game during the initial render (#5)

--- src/components/App/__test__/App.test.tsx
+++ src/components/App/__test__/App.test.tsx
@@ -1,8 +1,49 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { PlayerColor } from '../../../@types/coreTypes';
+import { PadId } from '../../../@types/padTypes';
 import App from '../App';
 
-it('renders the App component', () => {
-  render(<App />);
-  const divElement = screen.getByText(/App/i);
-  expect(divElement).toBeInTheDocument();
+describe("the game's initial phase", () => {
+  it('should have a background', () => {
+    render(<App />);
+    const bg = screen.getByTestId('background');
+    expect(bg).toBeInTheDocument();
+  });
+
+  it('should have a visible background', () => {
+    render(<App />);
+    const bg = screen.getByTestId('background');
+    expect(bg).toBeVisible();
+  });
+
+  it('should not have any cows on the board', () => {
+    render(<App />);
+    const cows = screen.queryAllByTestId(/^COW_[\d]{2}$/);
+    expect(cows.length).toBe(0);
+  });
+
+  it('should have 24 pads on the board', () => {
+    render(<App />);
+    const pads = screen.getAllByTestId(/^PAD_[\d]{2}$/);
+    expect(pads.length).toBe(24);
+    pads.forEach((pad) => {
+      expect(pad).toBeVisible();
+    });
+  });
+
+  it('should only have visible pads', () => {
+    render(<App />);
+    const pads = screen.getAllByTestId(/^PAD_[\d]{2}$/);
+    pads.forEach((pad) => {
+      expect(pad).toBeVisible();
+    });
+    console.log(pads[0].getAttribute('fill'));
+  });
+
+  it('should only render unvisited pads', () => {
+    render(<App />);
+    const cows = screen.queryAllByTestId(/^COW_[\d]{2}$/);
+    expect(cows.length).toBe(0);
+  });
 });
--- src/components/Cows/__test__/Cows.test.tsx
+++
@@ -1,5 +0,0 @@
-it('renders the Cows component', () => {
-  expect(true).toBe(true);
-});
-
-export {};
